814dbf2714967afd416ed9a2e9c4902533b861f0
JouCode <salazarjosue2604@gmail.com>
2022-10-13 00:46:40 -0500

    Update

--- whatsapp_api_main/controllers/controllers.py
+++ whatsapp_api_main/controllers/controllers.py
@@ -9,4 +9,86 @@ class WhatsappApiMain(http.Controller):
         data_request = request.env['whatsapp.api.main.requests'].sudo()
         data_users = request.env['whatsapp.api.main.users'].sudo()
         data_message = request.env['whatsapp.api.main.messages'].sudo()
-        return kw
+        data_message_line = request.env['whatsapp.api.main.messages.line'].sudo()
+
+        headers = request.httprequest.headers
+        data = request.jsonrequest
+
+        # Get ip of server
+        ip_address = request.httprequest.environ['REMOTE_ADDR']
+
+        # Get Values of request
+        authorization = headers['Authorization'].split(" ")[1]
+        data_base_name = data['db_name']
+        user_name = data['user_name']
+
+        verification_status = self.check_user_authenticate(data, headers, data_users)
+
+        if verification_status:
+            if data['type'] == 'authorization':
+                Response.status = '200'
+                data_response = {
+                    'status' : Response.status,
+                    'token' : verification_status.token,
+                    'url' : verification_status.url
+                }
+            elif data['type'] == 'save_message':
+                Response.status = '200'
+                data_response = {
+                    'Ok' : data['message_data']
+                }
+            else:
+                Response.status = '401'
+                data['type'] = 'Desconocido!'
+                data_response = {
+                    "Error" : data['type']
+                }
+        else:
+            Response.status = '401'
+            data_response = {
+                "Error" : Response.status
+            }
+
+        # Create registration of request
+        new_request = {
+            'name' : '',
+            'db_name' : data_base_name,
+            'ip_server' : ip_address,
+            'type_request' : data['type'],
+            'status_code' : Response.status
+        }
+        create_request = data_request.create(new_request)
+        if data['type'] == 'save_message':
+            # Create registration of messages
+            new_message = {
+                'name' : '',
+                'id_request' : create_request.id,
+                'messaging_product' : data['message_data']['messaging_product'],
+                'recipient_type' : data['message_data']['recipient_type'],
+                'recipient_id' : data['message_data']['recipient_id'],
+                'status_code' : data['message_data']['status_code']
+            }
+            create_message = data_message.create(new_message)
+            new_message_line = {
+                'message_id': create_message.id,
+                'type_message': data['message_data']['type_message'],
+                'type_message_body': data['message_data']['type_message_body'],
+                'text_message': data['message_data']['text_message'],
+                'type_message_header': data['message_data']['type_message_header'],
+                'link_file': data['message_data']['link_file'],
+                'filename': data['message_data']['filename']
+            }
+            data_message_line.create(new_message_line)
+        return data_response
+    
+    def check_user_authenticate(self, data, headers, data_users):
+        authorization = headers['Authorization'].split(" ")[1]
+        data_base_name = data['db_name']
+        user_name = data['user_name']
+        # Search user_account
+        account_user = data_users.search([('name', '=', user_name)])
+
+        if account_user.token_verify == authorization:
+            return account_user
+        else:
+            return False
--- whatsapp_api_main/models/models.py
+++ whatsapp_api_main/models/models.py
@@ -10,6 +10,7 @@ class WhatsAppRequests(models.Model):
     name = fields.Char(string="ID Requets", default="/",readonly=True)
     db_name = fields.Char(string="DataBase Name", require=True, readonly=True)
     ip_server = fields.Char(string="Direction IP of Servidor", require=True, readonly=True)
+    type_request = fields.Char(string="Tipo de Solicitud!")
     status_code = fields.Selection(selection=[('200', 'Done'),('401', 'Unauthorized')], string='Code Status Request', default='401', readonly=True)
 
     def create(self, vals):
@@ -35,7 +36,7 @@ class AccountsWABA(models.Model):
     phone_number_id = fields.Char(string="Id phone number", require=True)
     token = fields.Char(string="Token WABA", requiere=True)
     url = fields.Char(string="URL complete", compute='_get_url')
-
+    
     @api.depends('phone_number_id')
     def _get_url(self):
         self.url = f"https://graph.facebook.com/v14.0/{self.phone_number_id}/messages"
@@ -46,12 +47,26 @@ class WhatsAppMessages(models.Model):
     _description = 'Registro de Mensajes'
     _order = 'create_date desc'
 
-    name = fields.Many2one('whatsapp.api.main.users', string="Id Users Account", readonly=True)
+    name = fields.Char(string="Name Message", default="/",readonly=True)
     id_request = fields.Many2one('whatsapp.api.main.requests', string="Id request", readonly=True)
     messaging_product = fields.Char(string="Meta Service", default="whatsapp", readonly=True)
     recipient_type = fields.Char(string="Type of recipient", default="individual", readonly=True)
     recipient_id = fields.Char(string="Number of recipient", readonly=True)
-    status_code = fields.Selection(selection=[('200', 'Done'),('401', 'Unauthorized')], string='Code Status Request', default='401', readonly=True)
+    status_code = fields.Selection(selection=[('200', 'Done'),('401', 'Unauthorized'), ('err', 'Error')], string='Code Status Request', default='err', readonly=True)
+
+    def create(self, vals):
+        self._cr.execute("""
+            select	max(split_part(name,'/',2)::int)
+                            from whatsapp_api_main_messages where name != '/'
+        """)
+        max_num = self._cr.fetchone()
+        max_num = max_num[0]+1 if max_num[0] else 1
+        rec_name = 'Message MSG/'+str(max_num).zfill(3)
+        vals['name'] = rec_name
+        if vals['status_code'] != '200' and vals['status_code'] != '401':
+            vals['status_code'] = 'err'
+        return super(WhatsAppMessages, self).create(vals)
+
 
     def unlink(self):
         raise exceptions.Warning('No puedes eliminar registros de Mensajes!')
@@ -63,12 +78,11 @@ class WhatsAppMessagesLine(models.Model):
 
     message_id = fields.Many2one('whatsapp.api.main.messages', string="Id Message", readonly=True)
     type_message = fields.Char(string="Type of message send", readonly=True)
+    type_message_body = fields.Char(string="Type of message body", readonly=True)
     text_message = fields.Char(string="Data of message", readonly=True)
-    preview_url = fields.Boolean('URL preview', default=True, readonly=True)
+    type_message_header = fields.Char(string="Type of message header", readonly=True)
     link_file = fields.Char(string="URL of the File", readonly=True)
-    caption = fields.Char(string="Captio of message(Only Type Image or Video)", readonly=True)
     filename = fields.Char(string="Name of the file(Only document)", readonly=True)
 
     def unlink(self):
         raise exceptions.Warning('No puedes eliminar registros de Mensajes!')
-
